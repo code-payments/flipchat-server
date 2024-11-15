df5b998e459e01f8b05714fc246ab27bb923192c
Zelimir Fedoran <zelimir.fedoran@gmail.com>
2024-11-15 17:12:30 -0500

    account: removed comments

--- account/postgres/store.go
+++ account/postgres/store.go
@@ -15,31 +15,6 @@ type store struct {
 	client *db.PrismaClient
 }
 
-/*
-
-type Store interface {
-	// Bind binds a public key to a UserId, or returns the previously bound UserId.
-	Bind(ctx context.Context, userID *commonpb.UserId, pubKey *commonpb.PublicKey) (*commonpb.UserId, error)
-
-	// GetUserId returns the UserId associated with a public key.
-	//
-	/// ErrNotFound is returned if no binding exists.
-	GetUserId(ctx context.Context, pubKey *commonpb.PublicKey) (*commonpb.UserId, error)
-
-	// GetPubKeys returns the set of public keys associated with an account.
-	GetPubKeys(ctx context.Context, userID *commonpb.UserId) ([]*commonpb.PublicKey, error)
-
-	// RemoveKey removes a key from the set of user keys.
-	//
-	// It is idempotent, and does not return an error if the user does not exist.
-	RemoveKey(ctx context.Context, userID *commonpb.UserId, pubKey *commonpb.PublicKey) error
-
-	// IsAuthorized returns whether or not a pubKey is authorized to perform actions on behalf of the user.
-	IsAuthorized(ctx context.Context, userID *commonpb.UserId, pubKey *commonpb.PublicKey) (bool, error)
-}
-
-*/
-
 func NewPostgres(client *db.PrismaClient) account.Store {
 	return &store{
 		client,
