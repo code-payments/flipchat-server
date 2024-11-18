26145e31326da7244a52bd286c28dc9b4e179cc4
Zelimir Fedoran <zelimir.fedoran@gmail.com>
2024-11-18 14:40:58 -0500

    database: added new intent model and migration, added createdAt and updatedAt columns to all tables

---
+++ database/prisma/migrations/20241118193934_intent/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `flipchat_publickeys` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `flipchat_users` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "flipchat_publickeys" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "flipchat_users" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- CreateTable
+CREATE TABLE "flipchat_intnets" (
+    "id" TEXT NOT NULL,
+    "isFullfilled" BOOLEAN NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "flipchat_intnets_pkey" PRIMARY KEY ("id")
+);
