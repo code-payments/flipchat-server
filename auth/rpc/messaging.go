3c76ab8040689c7079e27059eb790282aac5ed57
Jeff Yanta <jeff@getcode.com>
2024-12-05 12:30:41 -0500

    auth: allow public read access to messages

--- auth/rpc/messaging.go
+++ auth/rpc/messaging.go
@@ -19,11 +19,11 @@ func NewMessagingRpcAuthorizer(chats chat.Store) *MessagingAuthorizer {
 }
 
 func (a *MessagingAuthorizer) CanStreamMessages(ctx context.Context, chatID *commonpb.ChatId, userID *commonpb.UserId) (bool, error) {
-	return a.chats.IsMember(ctx, chatID, userID)
+	return true, nil
 }
 
 func (a *MessagingAuthorizer) CanGetMessages(ctx context.Context, chatID *commonpb.ChatId, userID *commonpb.UserId) (bool, error) {
-	return a.chats.IsMember(ctx, chatID, userID)
+	return true, nil
 }
 
 func (a *MessagingAuthorizer) CanSendMessage(ctx context.Context, chatID *commonpb.ChatId, userID *commonpb.UserId) (bool, error) {
