e90222125e5502a597b799219acbf93e658df5fb
Jeff Yanta <jeff@getcode.com>
2024-12-13 00:54:26 -0500

    iap: OnPurchaseCompleted nows verifies receipt and sets registration status

--- iap/memory/server_test.go
+++ iap/memory/server_test.go
@@ -8,10 +8,20 @@ import (
 )
 
 func TestIAP_MemoryServer(t *testing.T) {
+	pub, priv, err := generateKeyPair()
+	if err != nil {
+		t.Fatalf("error generating key pair: %v", err)
+	}
+
+	verifier := NewMemoryVerifier(pub)
+	validReceiptFunc := func(msg string) string {
+		return generateValidReceipt(priv, msg)
+	}
+
 	accounts := account.NewInMemory()
 
 	// Provide a teardown function if necessary. Here it's no-op.
 	teardown := func() {}
 
-	tests.RunServerTests(t, accounts, teardown)
+	tests.RunServerTests(t, accounts, verifier, validReceiptFunc, teardown)
 }
--- iap/server.go
+++ iap/server.go
@@ -4,15 +4,21 @@ import (
 	"context"
 
 	"go.uber.org/zap"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	iappb "github.com/code-payments/flipchat-protobuf-api/generated/go/iap/v1"
 
+	"github.com/code-payments/flipchat-server/account"
 	"github.com/code-payments/flipchat-server/auth"
+	"github.com/code-payments/flipchat-server/model"
 )
 
 type Server struct {
-	log   *zap.Logger
-	authz auth.Authorizer
+	log      *zap.Logger
+	authz    auth.Authorizer
+	accounts account.Store
+	verifier Verifier
 
 	iappb.UnimplementedIapServer
 }
@@ -20,18 +26,47 @@ type Server struct {
 func NewServer(
 	log *zap.Logger,
 	authz auth.Authorizer,
+	accounts account.Store,
+	verifier Verifier,
 ) *Server {
 	return &Server{
-		log:   log,
-		authz: authz,
+		log:      log,
+		authz:    authz,
+		accounts: accounts,
+		verifier: verifier,
 	}
 }
 
+// todo: eventually we'll need to distinguish what was purchased
 func (s *Server) OnPurchaseCompleted(ctx context.Context, req *iappb.OnPurchaseCompletedRequest) (*iappb.OnPurchaseCompletedResponse, error) {
-	_, err := s.authz.Authorize(ctx, req, &req.Auth)
+	userID, err := s.authz.Authorize(ctx, req, &req.Auth)
 	if err != nil {
 		return nil, err
 	}
+
+	log := s.log.With(
+		zap.String("user_id", model.UserIDString(userID)),
+		zap.String("platform", req.Platform.String()),
+		zap.String("receipt", req.Receipt.Value),
+	)
+
+	// todo: check if the receipt was already consumed
+
+	isVerified, err := s.verifier.VerifyReceipt(ctx, req.Receipt.Value)
+	if err != nil {
+		log.Warn("Failed to verify receipt", zap.Error(err))
+		return nil, status.Error(codes.Internal, "failed to set registration flag")
+	} else if !isVerified {
+		return &iappb.OnPurchaseCompletedResponse{Result: iappb.OnPurchaseCompletedResponse_INVALID_RECEIPT}, nil
+	}
+
+	err = s.accounts.SetRegistrationFlag(ctx, userID, true)
+	if err != nil {
+		log.Warn("Failed to set registration flag", zap.Error(err))
+		return nil, status.Error(codes.Internal, "failed to set registration flag")
+	}
+
+	// todo: mark the receipt as being consumed
 
 	return &iappb.OnPurchaseCompletedResponse{}, nil
 }
