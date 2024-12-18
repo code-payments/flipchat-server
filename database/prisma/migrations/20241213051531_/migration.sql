9135f2927803dc8d433416f9f66a9aad60125541
Jeff Yanta <jeff@getcode.com>
2024-12-18 08:56:52 -0500

    database: update migration to add isRegistered flag to set true values for users with a display name

--- database/prisma/migrations/20241213051531_/migration.sql
+++ database/prisma/migrations/20241213051531_/migration.sql
@@ -1,2 +1,4 @@
 -- AlterTable
 ALTER TABLE "flipchat_users" ADD COLUMN     "isRegistered" BOOLEAN NOT NULL DEFAULT false;
+
+UPDATE TABLE "flipchat_users" SET "isRegistered" = true WHERE "displayName" IS NOT NULL;
