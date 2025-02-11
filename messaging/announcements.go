b2c4d5e7ea2eb804216bb045f95759e69f84ab59
Jeff Yanta <jeff@getcode.com>
2025-02-11 11:52:37 -0500

    messaging: add non-breaking spaces around kin amounts in announcements

--- messaging/announcements.go
+++ messaging/announcements.go
@@ -121,7 +121,7 @@ func NewMessagingFeeChangedAnnouncementContentBuilder(ctx context.Context, profi
 		return &messagingpb.Content{
 			Type: &messagingpb.Content_LocalizedAnnouncement{
 				LocalizedAnnouncement: &messagingpb.LocalizedAnnouncementContent{
-					KeyOrText: kinAmountPrinter.Sprintf("%s changed the Listener Message fee to ⬢ %d Kin", profile.DisplayName, codekin.FromQuarks(quarks)),
+					KeyOrText: kinAmountPrinter.Sprintf("%s changed the Listener Message fee to ⬢\u00A0%d\u00A0Kin", profile.DisplayName, codekin.FromQuarks(quarks)),
 				},
 			},
 		}, nil
